7fe70a593868685bf2e9abb7b2bfa5d7a8f8967a
shenxuebing <shenxuebing@vip.qq.com>
2026-04-28 12:44:11 +0800

    feat: 添加 Engine 框架 SM2 回调扩展
    - eng_local.h: 在 struct engine_st 中添加 SM2 回调字段
      (tass_flags, ssl_generate_master_secret, tls1_generate_key_block, convert_privkey)
    - eng_pkey.c: 添加 3 个 set 函数和 3 个调用函数
      使用 ERR_raise 替代 ENGINEerr 适配 OpenSSL 3.5.4
    - engine.h: 添加回调类型定义和函数声明

--- crypto/engine/eng_pkey.c
+++ crypto/engine/eng_pkey.c
@@ -21,6 +21,29 @@ int ENGINE_set_load_privkey_function(ENGINE *e,
     return 1;
 }
 
+#ifndef OPENSSL_NO_SM2
+int ENGINE_set_ssl_generate_master_secret_function(ENGINE *e,
+    ENGINE_SSL_GEN_MASTER_PTR ssl_gen_master_f)
+{
+    e->ssl_generate_master_secret = ssl_gen_master_f;
+    return 1;
+}
+
+int ENGINE_set_tls1_generate_key_block_function(ENGINE *e,
+    ENGINE_TLS1_GEN_KEY_BLOCK_PTR tls1_gen_key_block_f)
+{
+    e->tls1_generate_key_block = tls1_gen_key_block_f;
+    return 1;
+}
+
+int ENGINE_set_convert_privkey_function(ENGINE *e,
+    ENGINE_CONVERT_KEY_PTR convertpriv_f)
+{
+    e->convert_privkey = convertpriv_f;
+    return 1;
+}
+#endif
+
 int ENGINE_set_load_pubkey_function(ENGINE *e, ENGINE_LOAD_KEY_PTR loadpub_f)
 {
     e->load_pubkey = loadpub_f;
@@ -136,3 +159,64 @@ int ENGINE_load_ssl_client_cert(ENGINE *e, SSL *s,
     return e->load_ssl_client_cert(e, s, ca_dn, pcert, ppkey, pother,
                                    ui_method, callback_data);
 }
+
+#ifndef OPENSSL_NO_SM2
+int ENGINE_ssl_generate_master_secret(ENGINE *e, SSL *s,
+                                      unsigned char *pms, size_t pmslen,
+                                      int free_pms)
+{
+    if (e == NULL) {
+        ERR_raise(ERR_LIB_ENGINE, ERR_R_PASSED_NULL_PARAMETER);
+        return 0;
+    }
+    if (!CRYPTO_THREAD_write_lock(global_engine_lock))
+        return 0;
+    if (e->funct_ref == 0) {
+        CRYPTO_THREAD_unlock(global_engine_lock);
+        ERR_raise(ERR_LIB_ENGINE, ENGINE_R_NOT_INITIALISED);
+        return 0;
+    }
+    CRYPTO_THREAD_unlock(global_engine_lock);
+    if (!e->ssl_generate_master_secret) {
+        ERR_raise(ERR_LIB_ENGINE, ENGINE_R_NO_LOAD_FUNCTION);
+        return 0;
+    }
+    return e->ssl_generate_master_secret(e, s, pms, pmslen, free_pms);
+}
+
+int ENGINE_tls1_generate_key_block(ENGINE *e, SSL *s,
+                                   unsigned char *km, size_t num)
+{
+    if (e == NULL) {
+        ERR_raise(ERR_LIB_ENGINE, ERR_R_PASSED_NULL_PARAMETER);
+        return 0;
+    }
+    if (!CRYPTO_THREAD_write_lock(global_engine_lock))
+        return 0;
+    if (e->funct_ref == 0) {
+        CRYPTO_THREAD_unlock(global_engine_lock);
+        ERR_raise(ERR_LIB_ENGINE, ENGINE_R_NOT_INITIALISED);
+        return 0;
+    }
+    CRYPTO_THREAD_unlock(global_engine_lock);
+    if (!e->tls1_generate_key_block) {
+        ERR_raise(ERR_LIB_ENGINE, ENGINE_R_NO_LOAD_FUNCTION);
+        return 0;
+    }
+    return e->tls1_generate_key_block(e, s, km, num);
+}
+
+int ENGINE_convert_private_key(ENGINE *e, const char *pri, size_t pri_len,
+                               unsigned char *out_file, void *callback_data)
+{
+    if (e == NULL) {
+        ERR_raise(ERR_LIB_ENGINE, ERR_R_PASSED_NULL_PARAMETER);
+        return 0;
+    }
+    if (!e->convert_privkey) {
+        ERR_raise(ERR_LIB_ENGINE, ENGINE_R_NO_LOAD_FUNCTION);
+        return 0;
+    }
+    return e->convert_privkey(e, pri, pri_len, out_file, callback_data);
+}
+#endif
