5ccba4d22baa2f955beffb1ae13eacaf14f509f6
shenxuebing <shenxuebing@vip.qq.com>
2026-05-25 17:39:20 +0800

    fix: 1.默认不使用sdf provider

--- crypto/provider_predefined.c
+++ crypto/provider_predefined.c
@@ -22,7 +22,9 @@ OSSL_provider_init_fn ossl_legacy_provider_init;
 OSSL_provider_init_fn ossl_smtc_provider_init;
 # endif
 #endif
+#ifndef OPENSSL_NO_SDFPROV
 OSSL_provider_init_fn ossl_sdfprov_provider_init;
+#endif
 
 const OSSL_PROVIDER_INFO ossl_predefined_providers[] = {
 #ifdef FIPS_MODULE
@@ -36,7 +38,9 @@ const OSSL_PROVIDER_INFO ossl_predefined_providers[] = {
 # ifdef STATIC_LEGACY
     { "legacy", NULL, ossl_legacy_provider_init, NULL, 0 },
 # endif
+#ifndef OPENSSL_NO_SDFPROV
     { "sdfprov", NULL, ossl_sdfprov_provider_init, NULL, 0 },
+#endif
     { "base", NULL, ossl_base_provider_init, NULL, 0 },
     { "null", NULL, ossl_null_provider_init, NULL, 0 },
 #endif
